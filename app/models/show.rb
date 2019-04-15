a1d80f8921132da14dfa8604cc3f9690163337c1
Shawn Ren <shawnren527@gmail.com>
2019-04-15 14:01:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,10 +4,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
 
-  def build_network(arg = {:call_letters => "NBC"})
-    self.network.shows << self
-  end
-
 # binding.pry
 # 0
 end
